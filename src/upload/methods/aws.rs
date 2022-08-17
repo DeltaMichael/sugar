84faee565e256b9007f0ea7483762f33cbfc2d40
Dilyan Marinov <delta_michael@protonmail.com>
2022-08-17 15:59:07 +0300

    Comply with Cryowar path schema

--- src/upload/methods/aws.rs
+++ src/upload/methods/aws.rs
@@ -114,10 +114,13 @@ impl AWSMethod {
                 }
             }
         }
-
-        let link = format!("https://{}.s3.amazonaws.com/{}", bucket.name(), path_str);
-
-        Ok((asset_info.asset_id, link))
+        if asset_info.content_type == "image/png" {
+            let link = format!("https://nft.cryowar.com/images/{}", &asset_info.name);
+            Ok((asset_info.asset_id, link))
+        } else {
+            let link = format!("https://nft.cryowar.com/metadata/{}", &asset_info.name);
+            Ok((asset_info.asset_id, link))
+        }
     }
 }
 
